1122b1fa6b6ecfa1110fbb1a1905b6718ffd4e34
laura <lauralekeu13@gmail.com>
2020-09-15 12:54:51 +0200

    Initial commit
    Mise en place de la structure MVC et connexion à la db

---
+++ app/config/params.php
@@ -0,0 +1,16 @@
+<?php
+/*
+
+      .app/config/params.php
+
+*/
+
+// Constantes de connexion
+define('HOSTNAME', 'localhost');
+define('DBNAME', 'cleanBlog_2017');
+define('USERNAME', 'root');
+define('USERPWD', 'root');
+
+// Zones dynamiques du template
+$content = '';
+$title = '';
