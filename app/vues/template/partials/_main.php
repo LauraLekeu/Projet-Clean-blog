0ae6e81bc2fb60cf0af1d7596758beec359a6b57
laura <lauralekeu13@gmail.com>
2020-09-15 14:00:52 +0200

    Intégration du template
    Zones dynamiques + décompositions en partials

---
+++ app/vues/template/partials/_main.php
@@ -0,0 +1,16 @@
+<?php
+/*
+
+      .app/vues/template/partials/_main.php
+
+*/
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-8 col-md-10 mx-auto">
+      <?php echo $content; ?>
+  </div>
+</div>
+<hr>
+</main>
